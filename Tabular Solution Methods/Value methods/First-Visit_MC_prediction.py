c7f58c5b87e6be61f33f3cd403a8ecaac85db8c8
Rafael Cunha <rafaelcunha2013@gmail.com>
2021-10-04 17:11:13 +0200

    Implementation of Monte Carlo first-visit

--- Tabular Solution Methods/Value methods/First-Visit_MC_prediction.py
+++ Tabular Solution Methods/Value methods/First-Visit_MC_prediction.py
@@ -6,9 +6,54 @@ October 1, 2021
 """
 
 import numpy as np
+from numpy.random import default_rng
 
-def generate_policy(state_space, action_space):
-    pi = np.random.uniform(0, 1, [state_space, action_space])
+class BlackJack:
+    def __init__(self):
+        self.action_space.n = 4
+        self.state_space.n = 5
+        self.done = False
+        self.info = {}
+        self.state = 1
+        pass
+
+    def step(self, action):
+        return next_state, reward, self.done, self.info
+
+    def reset(self):
+        self.state = 1
+        self.done = False
+        return self.state
+
+class Agent:
+    def __init__(self, pi, env):
+        self.policy = pi
+        self.env = env
+
+    def decision(self, state):
+        probs = self.policy[state]
+        rng = default_rng()
+        action = rng.choice(np.arange(self.env.action_space.n, p=probs))
+        rng.choice
+        np.arange
+        return action
+
+def generate_episode(env, agent):
+    history = []
+    done = False
+    state = env.reset()
+    while not done:
+        action = agent.decision(state)
+        next_state, reward, done, info = env.step(action)
+        history.append((state, action, reward))
+        state = next_state
+    return history
+
+def generate_policy(env):
+    rng = default_rng()
+    # alpha is the weights of the dirichlet distribution
+    alpha = rng.integers(1, 10, env.action_space.n)
+    pi = rng.dirichlet(alpha, env.state_space.n)
     return pi
 
 
@@ -17,6 +62,7 @@ def first_visit_mc(pi):
     return = [None] * pi.shape[0]
     done = False
     while not done:
-
+        action = agent(state)
         next_state, reward, done, info = env.step(action)
+
 
